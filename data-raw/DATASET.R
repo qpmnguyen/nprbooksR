c6dd67d1c0f78106b9dda4dc725441b3348eabf9
Quang Nguyen <quangpmnguyen@gmail.com>
2022-01-03 17:38:14 -0500

    Final DATASET.R

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,24 +7,6 @@ library(RSelenium)
 library(glue)
 library(stringr)
 
-r_text <- get_robotstxt("https://apps.npr.org/best-books")
-r_parsed <- parse_robotstxt(r_text)
-delay <- r_parsed$crawl_delay
-if (nrow(delay) == 0) {
-    delay <- 5
-}
-# only local selenium works
-rev <- rsDriver(port = 4444L, browser = "firefox")
-driver <- rev$client
-
-yrs <- c(2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020)
-
-
-driver$navigate(glue("https://apps.npr.org/best-books/#view=list&year={yr}", yr = yr))
-
-webElem <- driver$findElement(using = "class", "book-count")
-n_texts <- webElem$getElementText()[[1]]
-
 get_book_detail <- function(yr, index){
     driver$navigate(glue("https://apps.npr.org/best-books/#year={yr}&book={index}", yr = yr, index = index))
     html <- driver$getPageSource()[[1]]
@@ -38,5 +20,41 @@ get_book_detail <- function(yr, index){
     return(tibble(author = author, tags = list(tags), blurb = text, blurb_auth = blurb_auth, img = img))
     gc()
 }
+
+
+r_text <- get_robotstxt("https://apps.npr.org/best-books")
+r_parsed <- parse_robotstxt(r_text)
+delay <- r_parsed$crawl_delay
+if (nrow(delay) == 0) {
+    delay <- 5
+}
+# only local selenium works
+rev <- rsDriver(port = 4444L, browser = "firefox")
+driver <- rev$client
+
+yrs <- c(2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020)
+
+yrs_list <- vector(mode = "list", length = length(yrs))
+
+for (i in seq_len(length(yrs))){
+    print(glue("Current in year {yr}", yr = yrs[i]))
+    driver$navigate(glue("https://apps.npr.org/best-books/#view=list&year={yr}", yr = yrs[i]))
+    webElem <- driver$findElement(using = "class", "book-count")
+    n_texts <- webElem$getElementText()[[1]]
+    books <- vector(mode = "list", length = n_texts)
+    for (j in seq_len(n_texts)){
+        if (j %% 10 == 0){
+            print(glue("Currently at {index} out of {total}", index = j, total = n_texts))
+        }
+        books[[j]] <- get_book_detail(yr = yrs[i], index = j)
+    }
+    books <- do.call(dplyr::bind_rows, books)
+    yrs_list[[i]] <- books
+}
+
+
+data <- do.call(dplyr::bind_rows, yrs_list)
+# TODO: Reviewer is nested under the section in text
+data$blurb_auth
 
 #usethis::use_data(DATASET, overwrite = TRUE)
